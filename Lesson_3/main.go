646bb1a18f712802dfc94889361da658d53b51ff
baster <pnedoshivin@gmail.com>
2022-06-10 14:37:14 +0300

    'initial'

--- Lesson_3/main.go
+++ Lesson_3/main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/basterrus/Golang_Level_2/Lesson_3/myPackage/myPackage"
+)
 
 func main() {
 	fmt.Println("HelloWorld")
+	myPackage.SayHello()
+
 }
